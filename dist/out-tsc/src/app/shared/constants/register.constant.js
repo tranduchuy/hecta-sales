04439970a6aa56f0afee9988a83aae159b52e957
HuyTD <vincenthuytd@gmail.com>
2019-03-23 22:57:41 +0700

    Build 23-03-2019

--- dist/out-tsc/src/app/shared/constants/register.constant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var RegisterConstants;
-(function (RegisterConstants) {
-    RegisterConstants.siteKeyCaptcha = '6LeyxX8UAAAAAOIGTpptvlMs6prD9zY7iSCxYEBb';
-})(RegisterConstants = exports.RegisterConstants || (exports.RegisterConstants = {}));
-//# sourceMappingURL=register.constant.js.map
