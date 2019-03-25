9a05c38b00058ebf6df7d376702d0ddc2eb16911
truongtq <truongtq@pascaliaasia.com>
2019-03-26 00:34:41 +0700

    add level to route element inside lead module

--- src/app/main/lead/lead-routing.module.ts
+++ src/app/main/lead/lead-routing.module.ts
@@ -1,17 +1,23 @@
-import {NgModule} from '@angular/core';
-import {Route, RouterModule} from '@angular/router';
-import {LeadListComponent} from './list/lead-list.component';
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { LeadListComponent } from './list/lead-list.component';
 import { LeadDetailsComponent } from './lead-details/lead-details.component';
 
 const routes: Route[] = [
   {
     path: '',
     component: LeadListComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
+    data: {
+      level: 1
+    }
   },
   {
     path: ':id',
-    component: LeadDetailsComponent
+    component: LeadDetailsComponent,
+    data: {
+      level: 2
+    }
   }
 ];
 
