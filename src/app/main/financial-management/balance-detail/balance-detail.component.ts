f7ba48b2af9c82b6ac7ff4848b7a397a7992f9c5
zzphoghy <doivjpxx@gmail.com>
2019-03-28 21:06:21 +0700

    implement fuse progressing bar service

--- src/app/main/financial-management/balance-detail/balance-detail.component.ts
+++ src/app/main/financial-management/balance-detail/balance-detail.component.ts
@@ -1,25 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 import {FinancialService} from '../shared/service/financial.service';
+import {FuseProgressBarService} from "../../../../@fuse/components/progress-bar/progress-bar.service";
+import {PageBaseComponent} from "../../../shared/components/base/page-base.component";
 
 @Component({
   selector: 'app-balance-detail',
   templateUrl: './balance-detail.component.html',
   styleUrls: ['./balance-detail.component.scss']
 })
-export class BalanceDetailComponent implements OnInit {
+export class BalanceDetailComponent extends PageBaseComponent implements OnInit {
 
   balance: Object = {};
 
-  constructor(private _financialService: FinancialService) {
+  constructor(
+    private _financialService: FinancialService,
+    private _fuseProgressBarService: FuseProgressBarService
+  ) {
+    super();
   }
 
   ngOnInit() {
-    this._financialService.getBalance().subscribe(
+    this._fuseProgressBarService.show();
+    const sub = this._financialService.getBalance().subscribe(
       (res: any) => {
         this.balance = res.data.user.balance;
-        console.log(this.balance)
       }
-    )
+    );
+    this.subscriptions.push(sub);
+    this._fuseProgressBarService.hide();
   }
 
 }
